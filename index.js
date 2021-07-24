0e7cfc008bc8992d4e4c675210ba5d60fd709cd7
Layna Oyervides <laynaoyervides@gmail.com>
2021-07-23 19:03:29 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+// Code your solution
+//const cards = ["Angela","Lauren","Carlos"];
+//function writeCards(cards) {
+  // for (let i=0; i < cards.length; i++){
+   //return (`Thank you, ${cards}, for the wonderful surprise gift!`);
+   // }
+//}
+//const newArr = cards.map(writeCards);
+//console.log(cards);
+//console.log(newArr);
+
+function writeCards(cards) {
+     let messages = [];
+    for (let i=0; i < cards.length; i++){
+    messages.push(`Thank you, ${cards}, for the wonderful surprise gift!`);
+    }
+   return messages;
+}
+    
+function countDown(int){
+    while (int>= 0)
+        {
+        console.log(int);
+        int--;
+    }
+    return int;
+}
+countDown(10);
